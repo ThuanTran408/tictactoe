a8346c37a9644f446f14a312f4da1c0c63eaef34
Thuan Tran <your_email@real_email.edu>
2022-07-24 15:09:09 -0700

    added intiial JS

---
+++ app.js
@@ -0,0 +1,46 @@
+const displayMessage = document.querySelector('displayMessage');
+
+const boxes = document.querySelectorAll('box');
+
+const reset = document.querySelector('resetButton');
+
+const winningConditions = 
+[
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2],
+];
+let options = ["", "", "", "", "", "", "", "", "", ];
+let currentPlayer = 'O';
+let playing = false;
+
+startGame();
+
+function startGame(){
+
+}
+
+function boxClicked() {
+
+}
+
+function updateBox(){
+
+}
+
+function switchPlayer(){
+
+}
+
+function checkWin(){
+
+}
+
+function resetGame(){
+    
+}
